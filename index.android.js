6499b89ed2cccaea6204e8b1622f39a4ee02a742
siarhei-luskanau <siarhei.luskanau@gmail.com>
2017-03-17 12:23:59 +0300

    Added ListView and items mapping

--- index.android.js
+++ index.android.js
@@ -5,17 +5,59 @@ import ReactNative, {
   AppRegistry,
   StyleSheet,
   Text,
-  Switch,
+  ListView,
   View
 } from 'react-native';
 import Firebase from "firebase";
 
+Firebase.initializeApp({
+ apiKey: "AIzaSyA5rK5JDZFjY5SpIL5FwlXhx8Sty-k9FBs",
+    authDomain: "fir-database-f6b5a.firebaseapp.com",
+    databaseURL: "https://fir-database-f6b5a.firebaseio.com",
+    storageBucket: "fir-database-f6b5a.appspot.com",
+    messagingSenderId: "443316178527"
+});
+
 export default class ReactNativeIotDoorbell extends Component {
+
+ constructor() {
+    super();
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+
+    this.devicesRef = Firebase.database().ref('doorbell_app/devices');
+    this.imagesRef = Firebase.database().ref('doorbell_app/images');
+ }
+
+  listenForDoorbellRef() {
+    this.devicesRef.on('value', (snapshot) => {
+      console.log(`Doorbell state:${snapshot.val()}`)
+
+      // get children as an array
+      var items = [];
+      snapshot.forEach((child) => {
+        items.push(child.val().device_id);
+      });
+
+      this.setState({dataSource: this.state.dataSource.cloneWithRows(items)});
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>} />
       </View>
     );
+  }
+
+  componentDidMount() {
+    this.listenForDoorbellRef();
   }
 }
 
