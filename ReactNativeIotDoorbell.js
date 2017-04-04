52404374180c639a4a68c2d2f15db9e13900ed77
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2017-04-04 12:59:23 +0300

    updated to support ios

---
+++ ReactNativeIotDoorbell.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import { StackNavigator } from 'react-navigation';
+import Firebase from "firebase";
+
+const DevicesListScreen = require('./components/DevicesListScreen');
+const ImagesListScreen = require('./components/ImagesListScreen');
+
+Firebase.initializeApp({
+ apiKey: "AIzaSyA5rK5JDZFjY5SpIL5FwlXhx8Sty-k9FBs",
+    authDomain: "fir-database-f6b5a.firebaseapp.com",
+    databaseURL: "https://fir-database-f6b5a.firebaseio.com",
+    storageBucket: "fir-database-f6b5a.appspot.com",
+    messagingSenderId: "443316178527"
+});
+
+const ReactNativeIotDoorbell = StackNavigator({
+  DevicesList: {screen: DevicesListScreen},
+  ImagesList: {screen: ImagesListScreen},
+});
+
+module.exports = ReactNativeIotDoorbell;
